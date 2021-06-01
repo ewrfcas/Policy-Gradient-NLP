b7de12f936027d56a4ffbd23e44472742343bb7c
ewrfcas <ccjdurandal422@163.com>
2021-06-01 22:50:34 +0800

    initial commit

--- temp.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-import torch
-
-x = torch.tensor([21, 3, 21, 21., 3., 23.], dtype=torch.float32)
-print(torch.softmax(x, dim=0))
-x = x - 1000
-print(torch.softmax(x, dim=0))
-
